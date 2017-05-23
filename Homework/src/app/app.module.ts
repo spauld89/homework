fba5067524641e1b8a5c0dd5e467c050aa8e73de
psokolov <psokolov@payneteasy.com>
2017-05-23 12:28:32 +0300

    added components, search service and material module

--- Homework/src/app/app.module.ts
+++ Homework/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
